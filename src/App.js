efe6459adba317c442523702506e6dbabd2a0172
Charles <chazylites@gmail.com>
2024-09-03 13:50:42 +0100

    Added navbar

--- src/App.js
+++ src/App.js
@@ -1,11 +1,12 @@
 import React, { useEffect } from 'react';
 import { useDispatch } from 'react-redux';
-import { BrowserRouter as Router, Routes, Route, NavLink } from 'react-router-dom';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import Rockets from './components/Rockets';
 import Missions from './components/Missions';
 import MyProfile from './components/MyProfile';
 import { fetchRockets } from './redux/rocketsSlice';
 import { fetchMissions } from './redux/missionsSlice';
+import Navbar from './components/Navbar'; // Import the Navbar component
 
 function App() {
   const dispatch = useDispatch();
@@ -17,26 +18,7 @@ function App() {
 
   return (
     <Router>
-      <nav>
-        <NavLink 
-          to="/" 
-          className={({ isActive }) => (isActive ? "active-link" : undefined)}
-        >
-          Rockets
-        </NavLink>
-        <NavLink 
-          to="/missions" 
-          className={({ isActive }) => (isActive ? "active-link" : undefined)}
-        >
-          Missions
-        </NavLink>
-        <NavLink 
-          to="/profile" 
-          className={({ isActive }) => (isActive ? "active-link" : undefined)}
-        >
-          My Profile
-        </NavLink>
-      </nav>
+      <Navbar /> {/* Add the Navbar component here */}
       <Routes>
         <Route path="/" element={<Rockets />} />
         <Route path="/missions" element={<Missions />} />
